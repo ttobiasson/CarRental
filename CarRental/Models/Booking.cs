770002e549c91e9ce076cf76bbb506e4685d7677
Tom <tobiassontom@outlook.com>
2020-11-07 14:29:12 +0100

    New Classes

---
+++ CarRental/Models/Booking.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CarRental.Models
+{
+    class Booking : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string bookingNumber;
+        private Customer customer;
+        private Vehicle vehicle;
+        private int date;
+
+    }
+}
---
+++ CarRental/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CarRental.Models
+{
+    class Customer : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private int personalIDnr;
+
+        public int PersonalIDnr
+        {
+            get { return personalIDnr; }
+            set { personalIDnr = value;
+                  OnPropertyChanged("PersonalIDnr");
+            }
+        }
+    }
+}
